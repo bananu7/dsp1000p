1a95c3a7e26fe0d3003ec645b7bbba237e01ed50
bananu7 <bananu7@o2.pl>
2023-02-25 14:39:44 +0100

    Added program selector widget

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,44 @@ import './App.css'
 import { listen } from '@tauri-apps/api/event'
 import { invoke } from '@tauri-apps/api'
 
+function ProgramSelector() {
+  const sendProgramChange = (programId: number) => {
+    console.log("sending program change", programId)
+    invoke('send_program_change', { value: programId });
+  };
+
+  const programUp = () => {
+    if (programId >= 99)
+      return;
+    setProgramId(id => id + 1);
+    sendProgramChange(programId + 1);
+  }
+
+  const programDown = () => {
+    if (programId <= 0)
+      return;
+    setProgramId(id => id - 1);
+    sendProgramChange(programId - 1);
+  }
+
+  const [programId, setProgramId] = useState(0);
+
+  return (
+    <div>
+      <span>{programId+1}</span>
+      <button onClick={programUp}>Up</button>
+      <button onClick={programDown}>Down</button>
+    </div>
+  );
+
+}
+
 function App() {
   const [count, setCount] = useState(0)
 
   const openMidi = () => {
     console.log("opening midi")
     invoke('open_midi_connection', { outputPortIdx: 1 });
-  };
-
-  const sendProgramChange = () => {
-    console.log("sending program change")
-    invoke('send_program_change', { value: 1 });
   };
 
   return (
@@ -25,11 +52,8 @@ function App() {
         <button onClick={() => openMidi()}>
           openMidi
         </button>
-
-        <button onClick={() => sendProgramChange()}>
-          send rogram change
-        </button>
         <p>Click to open midi</p>
+        <ProgramSelector />
       </div>
       <p className="read-the-docs">
         Yoo it works
