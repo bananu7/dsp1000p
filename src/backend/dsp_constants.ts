6dbb60becdfa609d2c1b9df1b06737df1941faf7
bananu7 <bananu7@o2.pl>
2023-02-25 19:06:15 +0100

    Added a partial map of parameter names

--- src/backend/dsp_constants.ts
+++ src/backend/dsp_constants.ts
@@ -70,3 +70,172 @@ In order to set the parameters, first Engine needs to be set to the appropriate 
 then edit a or b can be changed.
 
 */
+
+type Variation = {
+  variation: string
+}
+
+type ReverbParameters = Variation & {
+  editA: string,
+  editBL: string,
+  editBR: string
+}
+
+// Delay, Echo, Flanger, Pitch, Chorus
+type StereoParameters = Variation & {
+  stereo: true,
+  editA: string,
+  editB: string,
+}
+
+type SpecialReverbParameters = Variation & {
+  editA: string,
+  editB: string
+}
+
+type DualEffectParameter = Variation & {
+  editAL: string,
+  editAR: string,
+  editBL: string,
+  editBR: string
+}
+
+type ParameterData =
+  ReverbParameters |
+  StereoParameters |
+  SpecialReverbParameters |
+  DualEffectParameter;
+
+type ParametersData = {
+  [key: number]: ParameterData
+};
+
+export const parameterData: ParametersData = {
+  [EffectType.CATHEDRAL]: {
+    variation: "Reverb Time",
+    editA: "Diffusion",
+    editBL: "Early Reflections",
+    editBR: "High Multiply",
+  },
+
+  [EffectType.PLATE]: {
+    variation: "Reverb Time",
+    editA: "Pre Delay",
+    editBL: "Stereo Width",
+    editBR: "High Multiply",
+  },
+
+  [EffectType.SMALL_HALL]: {
+    variation: "Reverb Time",
+    editA: "Diffusion",
+    editBL: "Early Reflections",
+    editBR: "Stereo Width",
+  },
+
+  [EffectType.ROOM]: {
+    variation: "Reverb Time",
+    editA: "Pre Delay",
+    editB: "Wall Damp"
+  },
+
+  [EffectType.STUDIO]: {
+    variation: "Reverb Time",
+    editA: "Pre Delay",
+    editBL: "Early Reflections",
+    editBR: "Wall Damp"
+  },
+
+  [EffectType.CONCERT]: {
+    variation: "Reverb Time",
+    editA: "Pre Delay",
+    editBL: "Early Reflections",
+    editBR: "Diffusion"
+  },
+
+  [EffectType.STAGE]: {
+    variation: "Reverb Time",
+    editA: "Diffusion",
+    editBL: "Liveliness",
+    editBR: "Stereo Width",
+  },
+
+  [EffectType.VOCAL]: {
+    variation: "Reverb Time",
+    editA: "Pre Delay",
+    editBL: "Early Reflections",
+    editBR: "High Multiply",
+  },
+
+  [EffectType.PERCUSSION]: {
+    variation: "Reverb Time",
+    editA: "Diffusion",
+    editB: "High Frequency Decay"
+  },
+
+  [EffectType.DELAY]: {
+    variation: "Del. Coarse",
+    stereo: true,
+    editA: "Delay Time",
+    editB: "Feedback",
+  },
+
+  [EffectType.ECHO]: {
+    variation: "Del. Coarse",
+    stereo: true,
+    editA: "Delay Time",
+    editB: "Feedback",
+  },
+
+  [EffectType.GATED_REVERB]: {
+    variation: "Gate Time",
+    editA: "Density",
+    editB: "Gate Threshold",
+  },
+
+  [EffectType.REVERSE_REVERB]: {
+    variation: "Gate Time",
+    editA: "Pre Delay",
+    editB: "Gate Threshold",
+  },
+
+  [EffectType.VOCAL_DISTORTION]: {
+    variation: "Distortion Type",
+    editA: "Delay Time (x 10ms)",
+    editB: "Delay Mix",
+  },
+
+  [EffectType.ROTARY_SPEAKER]: {
+    variation: "Rotary Type",
+    editA: "Horn Speed",
+    editB: "Rotor Speed",
+  },
+
+  [EffectType.VOCODER]: {
+    variation: "Vocoder Type",
+    editA: "Vocoder Distortion",
+    editB: "Vocoder Sens",
+  },
+
+  [EffectType.PITCH]: {
+    variation: "", // todo?
+    stereo: true,
+    editA: "Cent",
+    editB: "Semi Tone",
+  },
+
+  [EffectType.FLANGER]: {
+    variation: "Mod. Frequency",
+    stereo: true,
+    editA: "Mod. Depth",
+    editB: "Mod. Feedback",
+  },
+
+  [EffectType.CHORUS]: {
+    variation: "Mod. Frequency",
+    stereo: true,
+    editA: "Mod. Delay",
+    editB: "Mod. Depth",
+  },
+
+  // TODO: fill in dual effets
+};
